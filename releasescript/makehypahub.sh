fa503afeca2e5f596f1daff467de4be48ad876be
Paul Merrill <paul.merrill@hypahub.com>
2021-07-14 20:03:15 -0700

    Port to Amazon Linux 2

--- releasescript/makehypahub.sh
+++ releasescript/makehypahub.sh
@@ -3,7 +3,14 @@ set -e
 
 VERSION=1.15.1-hypahub
 
-sudo apt install -y fakeroot libfuse-dev libtool rman
+if type apt >/dev/null 2>&1; then
+    sudo apt install -y fakeroot libfuse-dev libtool rman
+    suffix=ubuntu
+elif type yum >/dev/null 2>&1; then
+    sudo amazon-linux-extras install epel -y
+    sudo yum install -y autoconf automake fakeroot fuse-devel gcc git libtool
+    suffix=al2
+fi
 
 REPO="${0%/*}/.."
 cd "$REPO"
@@ -25,6 +32,6 @@ rm -fr build
     fakeroot "$REPO/releasescript/makerelease.sh" $VERSION
 )
 
-fakeroot tar -C build -czf bindfs-$VERSION.tar.gz bindfs
+fakeroot tar -C build -czf bindfs-$VERSION-$suffix.tar.gz bindfs
 
 rm -r build
